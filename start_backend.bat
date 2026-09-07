ae56e585efd4232ace9f134e227bf98cddc5cab8
santhoshkannan-dev <myecommercesyt@gmail.com>
2026-09-07 20:01:53 +0530

    feat: configure dedicated frontend repository

--- start_backend.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-echo Starting Django Backend Server...
-cd backend
-if exist venv\Scripts\activate.bat call venv\Scripts\activate
-python manage.py runserver 8000
--- start_backend.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-echo "Starting Django Backend Server..."
-cd backend
-source venv/bin/activate
-python manage.py runserver 8000
